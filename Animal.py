6ceba24e970b06e588caf58a1a12f5ac9491c309
Ana <anahyrachel@gmail.com>
2025-02-22 23:13:43 -0500

    Added five new functions.

--- Animal.py
+++ Animal.py
@@ -1,7 +1,20 @@
 class Animal:
-    def __init__(self): 
-        print("hello")
+    def __init__(self, name, species): 
+        self._name= name
+        self._species= species
+        print('Hello, I am', self._name, ',a', self._species)
 
     def talk(self):
-        print("hi")
+        print('I', self._name,'like to bark')
+    
+    def eat (self, food):
+        print ('I', self._name, 'like to eat' ,food )
 
+    def sleep (self, hours):
+        print ('I', self._name, 'like to sleep for' ,hours, 'hours')
+    
+    def play (self, toy):
+        print ('I', self._name, 'like to play with' ,toy)
+    
+    def breed (self, breed):
+        print ('I', self._name, 'am a', breed)
